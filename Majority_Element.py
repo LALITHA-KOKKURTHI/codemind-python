79fd9a65a21a289ed471c7bfa2098b8547ca2d9a
LALITHA-KOKKURTHI <96776984+LALITHA-KOKKURTHI@users.noreply.github.com>
2022-07-01 22:25:43 +0530

    x=int(input()) l=list(map(int,input().split())) n=[] c=0 for i in list(set(l)):     for j in l:         if j in l:             if i==j:              c+=1     n.append([c,i])     c=0 m=max(n) print(m[1])

---
+++ Majority_Element.py
@@ -0,0 +1,13 @@
+x=int(input())
+l=list(map(int,input().split()))
+n=[]
+c=0
+for i in list(set(l)):
+    for j in l:
+        if j in l:
+            if i==j:
+             c+=1
+    n.append([c,i])
+    c=0
+m=max(n)
+print(m[1])
