bab3d427380a26d0f05847a697a1ace323b75c55
LALITHA-KOKKURTHI <96776984+LALITHA-KOKKURTHI@users.noreply.github.com>
2022-07-21 14:08:55 +0530

    def self_dividing(n):     for d in str(n):         if d=='0' or n%int(d)>0:             return False     return True left=int(input()) right=int(input()) for n in range(left,right+1):     if self_dividing(n):         print(n,end=' ')

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,16 +1,10 @@
-a=int(input())
-b=int(input())
-l=0
-for i in range(a,b+1):
-  x=len(str(i))
-  c=i
-  while c!=0:
-    j=c%10
-    if j!=0 and i%j==0:
-       l+=1 
-    c= c//10
-  if l== x:
-    print( i,end= ' ')
-  l=0
-    
-
+def self_dividing(n):
+    for d in str(n):
+        if d=='0' or n%int(d)>0:
+            return False
+    return True
+left=int(input())
+right=int(input())
+for n in range(left,right+1):
+    if self_dividing(n):
+        print(n,end=' ')
